8f7ea9aa17a17c54516c105fbc40ed0b61bd4a38
dw <61549047+olrro@users.noreply.github.com>
2020-10-10 14:29:00 +0300

    Add files via upload

--- dump.sql
+++ dump.sql
@@ -1,25 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 09, 2020 at 02:50 PM
--- Server version: 10.3.22-MariaDB-log
--- PHP Version: 7.3.17
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `lab`
 --
+CREATE DATABASE IF NOT EXISTS `lab` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `lab`;
 
 -- --------------------------------------------------------
 
